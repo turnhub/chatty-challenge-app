f18f13dbe591e110fdc2e27917878d10ce26d670
Federico Meini <federico.meini@gmail.com>
2022-03-14 19:35:21 +0100

    Remove dependency on Phoenix/Absinthe

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,16 +1,15 @@
-import { ApolloClient, ApolloLink, createHttpLink, InMemoryCache, split } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache, split } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { getMainDefinition } from '@apollo/client/utilities';
-import { create as createAbsintheSocket } from "@absinthe/socket";
-import { createAbsintheSocketLink } from "@absinthe/socket-apollo-link";
-import { Socket as PhoenixSocket } from "phoenix";
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
 
 // Access Token
 const ACCESS_TOKEN = process.env.REACT_APP_API_TOKEN;
 
 // GrahpQL API endpoints
-const HTTP_ENDPOINT = "https://chatty.gigalixirapp.com/api";
-const WS_ENDPOINT = "wss://chatty.gigalixirapp.com/socket";
+const HTTP_ENDPOINT = "https://chatty.gigalixirapp.com/graphql";
+const WS_ENDPOINT = "wss://chatty.gigalixirapp.com/graphql/ws";
 
 // Create HTTP Apollo link.
 const httpLink = createHttpLink({ uri: HTTP_ENDPOINT });
@@ -28,16 +27,11 @@ const authLink = setContext((_, { headers }) => {
 // Add authorization header to Apollo HTTP link
 const authedHttpLink = authLink.concat(httpLink);
 
-// Create a standard Phoenix websocket connection.
-const phoenixSocket = new PhoenixSocket(WS_ENDPOINT, {
-    params: { Authorization: ACCESS_TOKEN ? `Bearer ${ACCESS_TOKEN}` : "" }
-});
-
-// Wrap the Phoenix socket in an AbsintheSocket.
-const absintheSocket = createAbsintheSocket(phoenixSocket);
-
 // Create Web Socket Apollo link.
-const wsLink = createAbsintheSocketLink(absintheSocket);
+const wsLink = new GraphQLWsLink(createClient({
+    url: WS_ENDPOINT,
+    connectionParams: { token: ACCESS_TOKEN },
+}));
 
 // Create a split Apollo link that uses the Web Socket link for subscriptions
 // and the HTTP link for queries and mutations
@@ -49,7 +43,7 @@ const splitLink = split(
             definition.operation === 'subscription'
         );
     },
-    wsLink as unknown as ApolloLink,
+    wsLink,
     authedHttpLink,
 );
 
