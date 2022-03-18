c2a7d2afeaefd687c0e49fa2f76811358f040b04
Federico Meini <federico.meini@gmail.com>
2022-03-18 11:55:03 +0100

    Show warning if API token is missing

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import SubscriptionExample from './SubscriptionExample';
 function App() {
   return (
     <div>
+      {!process.env.REACT_APP_API_TOKEN &&
+        <strong style={{ color: "red" }}>API TOKEN MISSING! Check instructions and it to your .env file.</strong>
+      }
+
       <h2>GraphQL Query example</h2>
       <QueryExample />
 
